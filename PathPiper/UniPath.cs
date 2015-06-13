22b023053ec4ea341ce2d08e0e843a3608376297
Zwirbelbart <simon8032000-git@yahoo.de>
2015-06-14 01:55:41 +0200

    switched from Path class to our GetDirectorySeperator method

--- PathPiper/UniPath.cs
+++ PathPiper/UniPath.cs
@@ -162,7 +162,7 @@ namespace PathPiper
         }
 
         //todo: char or string?
-        public static char EnvironmentDirectorySeperator { get { return Path.DirectorySeparatorChar; } }
+        public static char EnvironmentDirectorySeperator { get { return GetDirectorySeperator(EnvironmentPathStyle); } }
 
         public static string CurrentDirectory { get { return _currentDirectory; } }
 
