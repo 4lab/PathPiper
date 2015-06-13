457055cb0d7a3f9b8db63ef30a942a2684795247
Niklas Mollenhauer <nikeee@outlook.com>
2015-06-14 01:29:31 +0200

    Implement Name

--- PathPiper/UniPath.cs
+++ PathPiper/UniPath.cs
@@ -69,7 +69,14 @@ namespace PathPiper
 
         public string Name
         {
-            get { throw new NotImplementedException(); }
+            get
+            {
+                Debug.Assert(_directories != null);
+                if (_directories.Count == 0)
+                    return null;
+                var last = _directories[_directories.Count - 1];
+                return Path.GetDirectoryName(last);
+            }
         }
 
         public UniPath Normalize()
