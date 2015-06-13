5076413ce669daeeae251901688c6a56950ed004
Niklas Mollenhauer <nikeee@outlook.com>
2015-06-13 22:45:12 +0200

    Hello

---
+++ PathPiper.Tests/UniPathTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace PathPiper.Tests
+{
+    [TestFixture]
+    public class UniPathTests
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
