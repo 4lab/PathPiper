58be81a34b4099dbaf9635ed04e537e6a6a3cdb7
Niklas Mollenhauer <nikeee@outlook.com>
2018-12-06 13:22:47 +0100

    Use List<T> instead of Collection<T>

--- src/PathPiper/UniPath.cs
+++ src/PathPiper/UniPath.cs
@@ -13,11 +13,11 @@ namespace PathPiper
         private const string _parentDirectoryAbbreviation = "..";
         private static readonly char[] _invalidFileNameChars = { '\"', '<', '>', '|', '\0', (Char)1, (Char)2, (Char)3, (Char)4, (Char)5, (Char)6, (Char)7, (Char)8, (Char)9, (Char)10, (Char)11, (Char)12, (Char)13, (Char)14, (Char)15, (Char)16, (Char)17, (Char)18, (Char)19, (Char)20, (Char)21, (Char)22, (Char)23, (Char)24, (Char)25, (Char)26, (Char)27, (Char)28, (Char)29, (Char)30, (Char)31, ':', '*', '?', '\\', '/' };
 
-        private readonly ReadOnlyCollection<string> _directories;
+        private readonly IReadOnlyList<string> _directories;
 
-        protected UniPath(ReadOnlyCollection<string> directories)
+        protected UniPath(IReadOnlyList<string> directories)
         {
-            _directories = directories ?? new ReadOnlyCollection<string>(new string[0]);
+            _directories = directories ?? new List<string>(new string[0]);
         }
 
         public static UniPath Parse(string path) => Parse(path, EnvironmentPathStyle);
